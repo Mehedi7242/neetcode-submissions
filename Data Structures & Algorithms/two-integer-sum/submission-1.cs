ef50e2ab15ee7b068539562f924a2ed296a9635c
Mehedi <2121155@iub.edu.bd>
2026-06-17 13:08:58 +0600

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> lookUp = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int remaining = target - nums[i];
+
+            if (lookUp.ContainsKey(remaining))
+            {
+                int firstIndex = lookUp[remaining];
+                int secondIndex = i;
+
+                Console.WriteLine(firstIndex + " " + secondIndex);
+                return [firstIndex, secondIndex];
+
+            }
+
+            lookUp[nums[i]] = i;
+        }
+
+        return Array.Empty<int>();
+    }
+}
