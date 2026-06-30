b1c3de89db06058c5ef3fef3456f75723e9777d2
Mehedi <2121155@iub.edu.bd>
2026-06-30 16:09:56 +0600

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        Array.Sort(nums);
+
+        int expected = 1;
+
+        foreach (int x in nums) {
+            if (x < expected) {
+                continue;
+            } else if (x == expected) {
+                expected++;
+            } else  // x > expected
+            {
+                return expected;
+            }
+        }
+        return expected;
+    }
+}
